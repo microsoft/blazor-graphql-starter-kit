dc2c1caf773130bb7e820ecdb3200b55bfb51fbb
Sameer Doshi <sadoshi@microsoft.com>
2021-08-11 13:24:06 -0500

    Adding an additional query with a complex object.

--- graphqlapi/Resolvers/Query.cs
+++ graphqlapi/Resolvers/Query.cs
@@ -1,4 +1,10 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using HotChocolate;
+using HotChocolate.Types;
+using graphqlapi.Models;
+using System;
 
 namespace graphqlapi.Resolvers
 {
@@ -7,6 +13,23 @@ namespace graphqlapi.Resolvers
         public string Welcome(ClaimsPrincipal claimsPrincipal)
         {
             return $"Congrats on making your first query!. You are authenticated as: {claimsPrincipal.Identity.Name}";
+        }
+
+        public IList<Questionnaire> GetQuestionnairs()
+        {
+            return new List<Questionnaire>()
+            {
+                new Questionnaire()
+                {
+                    Title="Sample 1",
+                    Date=DateTime.Now,
+                },
+                new Questionnaire()
+                {
+                    Title="Sample 2",
+                    Date=DateTime.Now,
+                },
+            };
         }
     }
 }
